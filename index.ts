ea39f89abdb16133f60c3f3e2a447a5a51c96161
Ronaldo Cesar Torre <ronaldo.torre@econective.com.br>
2024-11-15 18:09:10 -0300

    Import project

---
+++ index.ts
@@ -0,0 +1,8 @@
+import Splash from '@/app';
+import { registerRootComponent } from 'expo';
+
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(Splash);
